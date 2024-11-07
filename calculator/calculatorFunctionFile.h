590248e6956497f5ad075f583f8677499114857b
NReilly4755 <nreilly4755@conestogac.on.ca>
2024-11-07 16:52:01 -0500

    Update 1.1
    -Added in the full source from my Conestoga GitHub repository where I originally made the project including the function header file, main file, and the file for the functions code.

---
+++ calculator/calculatorFunctionFile.h
@@ -0,0 +1,17 @@
+#pragma once
+//All function prototypes.
+void modeSelect(void);
+double firstNumber(void);
+double secondNumber(void);
+double addition(double, double);
+double subtraction(double, double);
+double multiplication(double, double);
+double division(double, double);
+int modulus(int, int);
+double exponents(double, double);
+double baseNumber(void);
+double toPower(void);
+double rootOf(void);
+double squareRoot(double);
+double newtonSquare(double);
+void cleanBuffer(void);
